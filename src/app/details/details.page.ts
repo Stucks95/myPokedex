f9467da8e2d1faa4b7025f56661ca0efc7e16c48
Stucks95 <Simone.Stacchiotti@regione.marche.it>
2023-10-11 13:12:05 +0200

    poke stats added

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -7,17 +7,18 @@ import { PokemonService } from '../services/pokemon.service';
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-
 export class DetailsPage implements OnInit {
 
   details: {
-    pokeIndex: number | null;
-    name: string;
-    gen: string;
-    description: string;
-    homeSprite: string;
-    sprites: Array<string>;
-    types: Array<string>
+    pokeIndex: number | null
+    name: string
+    gen: string
+    description: string
+    homeSprite: string
+    sprites: string[]
+    types: string[]
+    stats: { name: string, base_stat: number }[],
+    totStats: number | null
   } = {
     pokeIndex: null,
     name: '',
@@ -25,7 +26,9 @@ export class DetailsPage implements OnInit {
     description: '',
     homeSprite: '',
     sprites: [],
-    types: [] 
+    types: [],
+    stats: [],
+    totStats: null
   }
 
   constructor(
@@ -36,28 +39,33 @@ export class DetailsPage implements OnInit {
     // get INDEX
     this.details.pokeIndex = Number (this.route.snapshot.paramMap.get('index'))
 
-    this.pokeService.getPokeDetails(this.details.pokeIndex).subscribe((det: any) => {
+    this.pokeService.getPokeDetails(this.details.pokeIndex)
+    .subscribe((det: any) => {
       // get pokemon's name
-      this.details.name = det['name']
+      this.details.name = det.name
 
       // get home sprite
-      this.details.homeSprite = det['sprites']['other']['home']['front_default']
+      this.details.homeSprite = det.sprites.other.home.front_default
       
       // get the 4 sprites
-      this.details.sprites[0] = det['sprites']['front_default']
-      this.details.sprites[1] = det['sprites']['back_default']
-      this.details.sprites[2] = det['sprites']['front_shiny']
-      this.details.sprites[3] = det['sprites']['back_shiny']
+      this.details.sprites[0] = det.sprites.front_default
+      this.details.sprites[1] = det.sprites.back_default
+      this.details.sprites[2] = det.sprites.front_shiny
+      this.details.sprites[3] = det.sprites.back_shiny
 
       // get types
-      det['types'].forEach((type: any) => {
-        this.details.types.push(type['type']['name'])
-      });
-    })
+      det.types.forEach((type: any) => {
+        this.details.types.push(type.type.name)
+      })
 
-    this.pokeService.getSpecies(this.details.pokeIndex).subscribe((spec : any) => {
-      this.details.description = spec['flavor_text_entries'][21]['flavor_text']
-    });
+      //get stats
+      det.stats.forEach((s: any) => {
+        let cleanName = s.stat.name.replace('-', ' ')
+        this.details.stats.push({name: cleanName, base_stat: s.base_stat})
+        this.details.totStats += s.base_stat 
+      })
+      console.log(this.details.stats)
+    })
     
   }
 }
