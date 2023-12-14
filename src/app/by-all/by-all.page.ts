74a15abf98d65515a58a06a7a987b74829abd741
Stucks95 <Simone.Stacchiotti@regione.marche.it>
2023-12-14 14:14:23 +0100

    check the searchbar on byall page

--- src/app/by-all/by-all.page.ts
+++ src/app/by-all/by-all.page.ts
@@ -20,6 +20,7 @@ export class ByAllPage {
   
   pokemons: pokeBaseInfo[]
   allPokemons: pokeBaseInfo[]
+  appVersion: string = this.pokeService.appVersion
 
   skeletonLoad: boolean = true
   skeletonArray: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
